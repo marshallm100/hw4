378cef91a5a571bd22e89fc7d22079eb657fc5d9
Marshall Masterson <marshall.masterson@kellogg.northwestern.edu>
2025-06-05 21:52:14 +0000

    fixed image upload

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -12,8 +12,8 @@ class EntriesController < ApplicationController
     @entry["occurred_on"] = params["occurred_on"]
     @entry["place_id"] = params["place_id"]
         @entry["user_id"] = session["user_id"]
-        @entry.uploaded_image.attach(params["uploaded_image"])
     @entry.save
+        @entry.uploaded_image.attach(params["uploaded_image"])
     redirect_to "/places/#{@entry["place_id"]}"
   end
 
