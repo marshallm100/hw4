9d6281e9276204fbfdfacd88ade320e4a28c2e81
Marshall Masterson <marshall.masterson@kellogg.northwestern.edu>
2025-06-05 20:29:14 +0000

    updates related to logged in user

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,18 +5,28 @@ class PlacesController < ApplicationController
   end
 
   def show
+    @user = User.find_by({ "id" => session["user_id"] })
     @place = Place.find_by({ "id" => params["id"] })
     @entries = Entry.where({ "place_id" => @place["id"] })
   end
 
   def new
+    @user = User.find_by({ "id" => session["user_id"] })
   end
 
   def create
-    @place = Place.new
-    @place["name"] = params["name"]
-    @place.save
-    redirect_to "/places"
+    @user = User.find_by({ "id" => session["user_id"] })
+    if @user != nil
+      @place = Place.new
+      @place["name"] = params["name"]
+      @place.save
+      
+    else
+          flash["notice"] = "Login first."
+      
+    end
+      redirect_to "/places"
+
   end
 
 end
